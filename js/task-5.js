c572cb553ff89f31e51d317bf0f183ad9084926b
iudinant <131783168+iudinant@users.noreply.github.com>
2023-12-25 14:56:15 +0100

    made tasks 4-6

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const button = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+const body = document.body;
+
+button.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  spanColor.textContent = newColor;
+});
--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,44 @@
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
+
+
+function createBoxes(amount) {
+  const boxes = document.querySelector('#boxes');
+  const boxSize = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const newBox = document.createElement('div');
+    const boxColor = getRandomHexColor();
+
+    newBox.style.width = `${boxSize + i * 10}px`;
+    newBox.style.height = `${boxSize + i * 10}px`;
+    newBox.style.backgroundColor = boxColor;
+
+    boxes.appendChild(newBox);
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const createBtn = document.querySelector('[data-create]');
+  const destroyBtn = document.querySelector('[data-destroy]');
+  const input = document.querySelector('input[type="number"]');
+
+  createBtn.addEventListener('click', () => {
+    const amount = input.value;
+
+    if (amount < 1 || amount > 100) {
+      return;
+    }
+
+    createBoxes(amount);
+    input.value = '';
+  });
+
+  destroyBtn.addEventListener('click', () => {
+    const boxes = document.querySelector('#boxes');
+    boxes.innerHTML = '';
+  });
+});
